43b650c3341f188bab9e27273988a7a438036626
Ma7th3w <273913713+Ma7th3w@users.noreply.github.com>
2026-04-24 12:59:06 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'filtercoffee-v2.5.6.1';
+const CACHE_NAME = 'filtercoffee-v2.5.6.2';
 const ASSETS = ['./index.html', './manifest.json'];
 
 self.addEventListener('install', e => {
